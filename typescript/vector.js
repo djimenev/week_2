f9f0e18a08e84e85a79bfa498a91fc5a86c44f1f
djimenev <diegojv10@hotmail.com>
2022-07-20 22:24:48 +0200

    nuevo commit

---
+++ typescript/vector.js
@@ -0,0 +1,57 @@
+var Vector = /** @class */ (function () {
+    function Vector(n, k) {
+        this.elements = [];
+        for (var x = 0; x < n; x++) {
+            this.elements.push(Number((Math.random() * k).toFixed()));
+        }
+        this.length = this.elements.length;
+    }
+    Vector.prototype.print = function () {
+        console.log(this.elements);
+    };
+    Vector.prototype.add = function (v1) {
+        var array = new Vector(0, 0);
+        if (this.length == v1.length) {
+            for (var x = 0; x < this.length; x++) {
+                //array[x] = v1[x] + this.elements[x];
+                array.elements.push(this.elements[x] + v1[x]);
+            }
+        }
+        return array;
+    };
+    Vector.prototype.subs = function (v1) {
+        var array = new Vector(0, 0);
+        if (this.length == v1.length) {
+            for (var x = 0; x < this.length; x++) {
+                array[x] = v1[x] - this.elements[x];
+            }
+        }
+        return array;
+    };
+    Vector.prototype.mult = function (v1) {
+        var array = new Vector(0, 0);
+        if (this.length == v1.length) {
+            for (var x = 0; x < this.length; x++) {
+                array[x] = v1[x] * this.elements[x];
+            }
+        }
+        return array;
+    };
+    Vector.prototype.multNumber = function (n) {
+        var array = new Vector(0, 0);
+        for (var x = 0; x < this.length; x++) {
+            array.elements.push(this.elements[x] * n);
+        }
+        array.length = array.elements.length;
+        return array;
+    };
+    return Vector;
+}());
+var vector1 = new Vector(10, 7);
+var vector2 = new Vector(10, 10);
+//console.log(vector1);
+//vector1.print();
+console.log(vector1);
+console.log(vector2);
+//console.log(vector1.multNumber(3));
+console.log(vector1.add(vector2));
---
+++ typescript/vector.ts
@@ -0,0 +1,83 @@
+class Vector{
+
+    private elements:number[];
+    public length:number;
+
+    constructor(n:number, k:number){
+        this.elements = [];
+        for(let x = 0;x<n ; x++){
+            this.elements.push(Number((Math.random() * k).toFixed()))
+        }
+        this.length = this.elements.length;
+
+        }
+
+        public print():void{
+            console.log(this.elements);
+            
+        }
+
+        public add(v1:Vector):Vector{
+            let array: Vector = new Vector(0,0);
+
+            if (this.length == v1.length) {
+              for (let x = 0; x < this.length; x++) {
+                //array[x] = v1[x] + this.elements[x];
+                //array.elements.push(this.elements[x] + v1[x]);
+              }
+        
+            }
+            return array;
+        }
+
+        public subs(v1:Vector):Vector{
+            let array: Vector = new Vector(0,0);
+
+            if (this.length == v1.length) {
+              for (let x = 0; x < this.length; x++) {
+                array[x] = v1[x] - this.elements[x];
+              }
+        
+            }
+            return array;
+        }
+
+        public mult(v1:Vector):Vector{
+            let array: Vector = new Vector(0,0);
+
+            if (this.length == v1.length) {
+              for (let x = 0; x < this.length; x++) {
+                array[x] = v1[x] * this.elements[x];
+              }
+        
+            }
+            return array;
+        }
+
+        public multNumber(n:number):Vector{
+            let array: Vector = new Vector(0,0);
+
+              for (let x = 0; x < this.length; x++) {
+                array.elements.push(this.elements[x] * n);
+              }
+              array.length = array.elements.length;
+              return array;
+            }
+           
+        }
+
+
+
+let vector1 = new Vector(10,7);
+let vector2 = new Vector(10,10);
+//console.log(vector1);
+//vector1.print();
+console.log(vector1);
+console.log(vector2);
+//console.log(vector1.multNumber(3));
+//console.log(vector1.add(vector2));
+
+
+
+
+
